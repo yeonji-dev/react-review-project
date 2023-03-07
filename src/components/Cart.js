f2c2aeedcc57aa7b0e40715e627e534cc85476b2
yeonji-dev <lilianaaa804@gmail.com>
2023-03-07 11:07:17 +0900

    Redux state object 일경우 변경

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,25 +1,36 @@
 import {Table} from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import {changeUser, changeNum} from "../store.js";
 
 const Cart = () => {
-    const storeState = useSelector((state) => state.user);
-    console.log(storeState);
+    const state = useSelector((state) => state);
+    const dispatch = useDispatch();
+
     return (
         <Table>
             <thead>
             <tr>
                 <th>#</th>
                 <th>상품명</th>
-                <th>수량</th>
-                <th>변경하기</th>
+                <th>
+                    <button onClick={() => {
+                            dispatch(changeNum(10));
+                        }}>숫자 바꾸기
+                    </button>
+                </th>
+                <th>
+                    <button onClick={() => {
+                        dispatch(changeUser('연지'));
+                    }}>이름 바꾸기</button>
+                </th>
             </tr>
             </thead>
             <tbody>
             <tr>
                 <td>1</td>
                 <td>안녕</td>
-                <td>안녕</td>
-                <td>안녕</td>
+                <td>{state.user.number}</td>
+                <td>{state.user.user}</td>
             </tr>
             </tbody>
         </Table>
--- src/store.js
+++ src/store.js
@@ -2,7 +2,25 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const user = createSlice({
     name: 'user',
-    initialState: 'Liliana',
+    initialState: {user: 'Liliana', number: 0},
+    reducers: {
+        changeUser(state, action){
+            state.user = action.payload;
+        },
+        changeNum(state, action){
+            state.number += action.payload;
+        },
+    }
+});
+
+const cartItem = createSlice({
+    name: 'cart',
+    initialState: [],
+    reducers: {
+        addItem(state, action){
+            state.push(action.payload);
+        }
+    }
 })
 
 export default configureStore({
@@ -10,3 +28,5 @@ export default configureStore({
         user: user.reducer
     }
 })
+
+export let { changeUser, changeNum } = user.actions;//변수명에 export 하고싶은 함수명 나열
